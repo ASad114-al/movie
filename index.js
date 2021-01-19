1df92b3c37f7b7d785f77bdade8da7f08015484f
Asad114-al <as3ad.94aa@gmail.com>
2021-01-19 14:40:16 +0100

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.dburi, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('Connected to my DB')
+        app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT}`))
+    })
+    .catch(err => console.log(err))
