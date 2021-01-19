ec623dbc0278cf670d945651116dfea782bc5395
Asad114-al <as3ad.94aa@gmail.com>
2021-01-19 14:58:35 +0100

    items

--- models/items.js
+++ models/items.js
@@ -2,26 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const movieItemSchema = new Schema({
-    url: {
-    type: String,
-    required: true
-    },
-    articlename: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: String,
-        required: true
-    },
-    company: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    }
+   
 }, {timestamps: true})
 
 const movieItem = mongoose.model('Designshop', movieItemSchema)
